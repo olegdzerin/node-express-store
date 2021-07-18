f43841efa68f516b92adf17f3b3188f98007b3c2
olegdzerin <oleg.dz2171@gmail.com>
2021-07-18 11:40:53 +0300

    start

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var authRouters = require('./routes/authRouters');
 var homeRouters = require('./routes/homeRouters');
 var productsRouters = require('./routes/productsRouters');
 
- var ordersRouters = require('./routes/ordersRouters');
- var customersRouters = require('./routes/customersRouters');
-
+var ordersRouters = require('./routes/ordersRouters');
+var customersRouters = require('./routes/customersRouters');
+const path = require('path')
 
 const cookieParser = require('cookie-parser')
 const {
@@ -19,12 +19,14 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })) ;
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(cookieParser())
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 // const dbURI = require('./env');
 
-    
+
 const {
   pool
 } = require('./require');
@@ -32,20 +34,23 @@ const {
 pool.connect()
   .then(() => {
     app.listen(3000),
-    console.log('Connection has been established successfully.....');
-    
+      console.log('Connection has been established successfully.....');
+
   })
   .catch((error) => {
     console.error('Unable to connect to the database:', error);
   })
 
- app.get('*', checkUser);
+app.get('*', checkUser);
 
-   app.get('/', (req, res) => res.redirect('/home'))
-  app.use('/home', homeRouters)
-app.use('/products',requireAuth,  productsRouters);
+app.get('/', (req, res) => res.redirect('/home'))
+app.use('/home', homeRouters)
+app.use('/products', requireAuth, productsRouters);
 app.use('/orders', requireAuth, ordersRouters);
 app.use('/customers', requireAuth, customersRouters);
 app.use(authRouters);
-
-
+app.get('*', (req, res) => {
+  console.log(__dirname);
+   res.sendFile(path.join(__dirname, 'public/index.html'));
+ 
+})
